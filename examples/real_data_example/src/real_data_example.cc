75718bc753e64a625aad72fe5b0a7ab1827ee637
blasscoc <blasscoc@gmail.com>
2024-11-14 09:28:10 -0600

    fix: updates broken include

--- examples/real_data_example/src/real_data_example.cc
+++ examples/real_data_example/src/real_data_example.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include < mdio / mdio.h>
+#include <mdio/mdio.h>
 
 #include <indicators/cursor_control.hpp>
 #include <indicators/indeterminate_progress_bar.hpp>
