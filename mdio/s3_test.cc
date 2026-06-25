4ddc7b4d9c47d6efe37c3fb097effda408817335
BrianMichell <brianm314@comcast.net>
2026-06-25 18:20:14 +0000

    Remove old v2/v3 shims

--- mdio/s3_test.cc
+++ mdio/s3_test.cc
@@ -52,128 +52,11 @@ std::string GetS3PathSuffix(mdio::zarr::ZarrVersion version) {
 }
 
 /**
- * @brief Returns the manifest without struct arrays (compatible with both V2
- * and V3).
+ * @brief Returns a manifest exercising both scalar and struct-array variables.
+ * Both V2 and V3 support the struct array (image_headers).
  */
 std::string GetSimpleManifest() {
   return R"(
-{
-    "metadata": {
-        "name": "campos_3d",
-        "apiVersion": "1.0.0",
-        "createdOn": "2023-12-12T15:02:06.413469-06:00",
-        "attributes": {
-            "textHeader": [
-                "C01 .......................... ",
-                "C02 .......................... ",
-                "C03 .......................... "
-            ],
-            "foo": "bar"
-        }
-    },
-    "variables": [
-    {
-        "name": "image",
-        "dataType": "float32",
-        "dimensions": [
-            {"name": "inline", "size": 256},
-            {"name": "crossline", "size": 512},
-            {"name": "depth", "size": 384}
-        ],
-        "metadata": {
-            "chunkGrid": {
-                "name": "regular",
-                "configuration": { "chunkShape": [128, 128, 128] }
-            },
-            "statsV1": {
-            "count": 100,
-            "sum": 1215.1,
-            "sumSquares": 125.12,
-            "min": 5.61,
-            "max": 10.84,
-            "histogram": {"binCenters":  [1, 2], "counts":  [10, 15]}
-            },
-            "attributes": {
-                "fizz": "buzz"
-            }
-        },
-        "coordinates": ["inline", "crossline", "depth", "cdp-x", "cdp-y"],
-        "compressor": {"name": "blosc", "algorithm": "zstd"}
-    },
-    {
-        "name": "velocity",
-        "dataType": "float64",
-        "dimensions": ["inline", "crossline", "depth"],
-        "metadata": {
-            "chunkGrid": {
-                "name": "regular",
-                "configuration": { "chunkShape": [128, 128, 128] }
-            },
-            "unitsV1": {"speed": "m/s"}
-        },
-        "coordinates": ["inline", "crossline", "depth", "cdp-x", "cdp-y"]
-    },
-    {
-        "name": "image_inline",
-        "dataType": "int16",
-        "dimensions": ["inline", "crossline", "depth"],
-        "longName": "inline optimized version of 3d_stack",
-        "compressor": {"name": "blosc", "algorithm": "zstd"},
-        "metadata": {
-            "chunkGrid": {
-                "name": "regular",
-                "configuration": { "chunkShape": [128, 128, 128] }
-            }
-        },
-        "coordinates": ["inline", "crossline", "depth", "cdp-x", "cdp-y"]
-    },
-    {
-        "name": "inline",
-        "dataType": "uint32",
-        "dimensions": [{"name": "inline", "size": 256}]
-    },
-    {
-        "name": "crossline",
-        "dataType": "uint32",
-        "dimensions": [{"name": "crossline", "size": 512}]
-    },
-    {
-        "name": "depth",
-        "dataType": "uint32",
-        "dimensions": [{"name": "depth", "size": 384}],
-        "metadata": {
-            "unitsV1": { "length": "m" }
-        }
-    },
-    {
-        "name": "cdp-x",
-        "dataType": "float32",
-        "dimensions": [
-            {"name": "inline", "size": 256},
-            {"name": "crossline", "size": 512}
-        ],
-        "metadata": {
-            "unitsV1": { "length": "m" }
-        }
-    },
-    {
-        "name": "cdp-y",
-        "dataType": "float32",
-        "dimensions": [
-            {"name": "inline", "size": 256},
-            {"name": "crossline", "size": 512}
-        ],
-        "metadata": {
-            "unitsV1": { "length": "m" }
-        }
-    }]
-}
-)";
-}
-
-// V2-only manifest with struct arrays
-
-/*NOLINT*/ std::string const fullToyManifest = R"(
 {
     "metadata": {
         "name": "campos_3d",
@@ -305,151 +188,6 @@ std::string GetSimpleManifest() {
     }]
 }
 )";
-
-TEST(S3, create) {
-  if (S3_PATH == "s3://USER_BUCKET/mdio_cpp_testing") {
-    GTEST_SKIP() << "Please set the S3_PATH to your own bucket in the "
-                    "s3_test.cc file.";
-  }
-  nlohmann::json j = nlohmann::json::parse(fullToyManifest);
-  auto dataset =
-      mdio::Dataset::from_json(j, S3_PATH, mdio::constants::kCreateClean);
-  EXPECT_TRUE(dataset.status().ok()) << dataset.status();
-}
-
-TEST(S3, open) {
-  if (S3_PATH == "s3://USER_BUCKET/mdio_cpp_testing") {
-    GTEST_SKIP() << "Please set the S3_PATH to your own bucket in the "
-                    "s3_test.cc file.";
-  }
-  nlohmann::json j = nlohmann::json::parse(fullToyManifest);
-  auto dataset = mdio::Dataset::Open(S3_PATH, mdio::constants::kOpen);
-  EXPECT_TRUE(dataset.status().ok())
-      << dataset.status();  // TODO(BrianMichell): How will timeouts work with
-                            // this? Can we simulate it or make it excessively
-                            // short to force one?
-}
-
-TEST(S3, write) {
-  if (S3_PATH == "s3://USER_BUCKET/mdio_cpp_testing") {
-    GTEST_SKIP() << "Please set the S3_PATH to your own bucket in the "
-                    "s3_test.cc file.";
-  }
-  nlohmann::json j = nlohmann::json::parse(fullToyManifest);
-  auto dataset = mdio::Dataset::Open(S3_PATH, mdio::constants::kOpen);
-  ASSERT_TRUE(dataset.status().ok()) << dataset.status();
-
-  auto ds = dataset.value();
-  std::vector<std::string> names = ds.variables.get_keys();
-
-  // Construct a vector of Variables to work with
-  std::vector<mdio::Variable<>> openVariables;
-  for (auto& key : names) {
-    auto var = ds.get_variable(key);
-    openVariables.emplace_back(var.value());
-  }
-
-  // Now we can start opening all the Variables
-  std::vector<mdio::Future<mdio::VariableData<>>> readVariablesFutures;
-  for (auto& v : openVariables) {
-    auto read = v.Read();
-    readVariablesFutures.emplace_back(read);
-  }
-
-  // Now we make sure all the reads were successful
-  std::vector<mdio::VariableData<>> readVariables;
-  for (auto& v : readVariablesFutures) {
-    ASSERT_TRUE(v.status().ok()) << v.status();
-    readVariables.emplace_back(v.value());
-  }
-
-  for (auto variable : readVariables) {
-    std::string name = variable.variableName;
-    mdio::DataType dtype = variable.dtype();
-    if (dtype == mdio::constants::kFloat32 && name == "image") {
-      auto data = reinterpret_cast<mdio::dtypes::float32_t*>(
-          variable.get_data_accessor().data());
-      data[0] = 3.14f;
-    } else if (dtype == mdio::constants::kFloat64 && name == "velocity") {
-      auto data = reinterpret_cast<mdio::dtypes::float64_t*>(
-          variable.get_data_accessor().data());
-      data[0] = 2.71828;
-    } else if (dtype == mdio::constants::kInt16 && name == "image_inline") {
-      auto data = reinterpret_cast<mdio::dtypes::int16_t*>(
-          variable.get_data_accessor().data());
-      data[0] = 0xff;
-    } else if (dtype == mdio::constants::kByte && name == "image_headers") {
-      auto data = reinterpret_cast<mdio::dtypes::byte_t*>(
-          variable.get_data_accessor().data());
-      for (int i = 0; i < 12; i++) {
-        data[i] = std::byte(0xff);
-      }
-    } else if (name == "inline") {
-      auto data = reinterpret_cast<mdio::dtypes::uint32_t*>(
-          variable.get_data_accessor().data());
-      for (uint32_t i = 0; i < 256; ++i) {
-        data[i] = i;
-      }
-    } else if (name == "crossline") {
-      auto data = reinterpret_cast<mdio::dtypes::uint32_t*>(
-          variable.get_data_accessor().data());
-      for (uint32_t i = 0; i < 512; ++i) {
-        data[i] = i;
-      }
-    } else if (name == "depth") {
-      auto data = reinterpret_cast<mdio::dtypes::uint32_t*>(
-          variable.get_data_accessor().data());
-      for (uint32_t i = 0; i < 384; ++i) {
-        data[i] = i;
-      }
-    }
-  }
-
-  // Pair the Variables to the VariableData objects via name matching so we can
-  // write them out correctly This makes an assumption that the vectors are 1-1
-  std::map<std::size_t, std::size_t> variableIdxPair;
-  for (std::size_t i = 0; i < openVariables.size(); i++) {
-    for (std::size_t j = 0; j < readVariables.size(); j++) {
-      if (openVariables[i].get_variable_name() ==
-          readVariables[j].variableName) {
-        variableIdxPair[i] = j;
-        break;
-      }
-    }
-  }
-
-  // Now we can write the Variables back to the store
-  std::vector<mdio::WriteFutures> writeFutures;
-  for (auto& idxPair : variableIdxPair) {
-    auto write =
-        openVariables[idxPair.second].Write(readVariables[idxPair.first]);
-    writeFutures.emplace_back(write);
-  }
-
-  // Now we make sure all the writes were successful
-  for (auto& w : writeFutures) {
-    ASSERT_TRUE(w.status().ok()) << w.status();
-  }
-}
-
-TEST(S3, read) {
-  if (S3_PATH == "s3://USER_BUCKET/mdio_cpp_testing") {
-    GTEST_SKIP() << "Please set the S3_PATH to your own bucket in the "
-                    "s3_test.cc file.";
-  }
-  nlohmann::json j = nlohmann::json::parse(fullToyManifest);
-  auto dataset = mdio::Dataset::Open(S3_PATH, mdio::constants::kOpen);
-  ASSERT_TRUE(dataset.status().ok()) << dataset.status();
-
-  auto ds = dataset.value();
-
-  for (auto& kv : ds.coordinates) {
-    std::string key = kv.first;
-    auto var = ds.get_variable(key);
-    ASSERT_TRUE(var.status().ok()) << var.status();
-  }
-  auto future = ds.SelectField("image_headers", "cdp-x");
-  ASSERT_TRUE(future.status().ok()) << future.status();
 }
 
 // ============================================================================
@@ -532,6 +270,24 @@ TEST_P(S3VersionTest, readWrite) {
 
   EXPECT_FLOAT_EQ(rereadAccessor[0], 3.14f);
   EXPECT_FLOAT_EQ(rereadAccessor[1], 2.71f);
+}
+
+TEST_P(S3VersionTest, selectField) {
+  if (S3_PATH == "s3://USER_BUCKET/mdio_cpp_testing") {
+    GTEST_SKIP() << "Please set the S3_PATH to your own bucket in the "
+                    "s3_test.cc file.";
+  }
+  nlohmann::json j = nlohmann::json::parse(GetSimpleManifest());
+  auto createRes = mdio::Dataset::from_json(j, test_path_, version_,
+                                            mdio::constants::kCreateClean);
+  ASSERT_TRUE(createRes.status().ok()) << createRes.status();
+
+  auto dataset = mdio::Dataset::Open(test_path_, mdio::constants::kOpen);
+  ASSERT_TRUE(dataset.status().ok()) << dataset.status();
+  auto ds = dataset.value();
+
+  auto future = ds.SelectField("image_headers", "cdp-x");
+  ASSERT_TRUE(future.status().ok()) << future.status();
 }
 
 INSTANTIATE_TEST_SUITE_P(
